2a9a5f14fa1f0334094e5ac5c465e1352c9e4fd3
umachandrannew <56246870+umachandrannew@users.noreply.github.com>
2019-10-09 11:02:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DBCommonADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CRUD operation
+
+            //CREATE
+            try
+            {
+                using (DataConnection d = new DataConnection("studentconn"))
+                {
+                    string sqlInsert = "insert into student_details values ('PHIL','M','CRICKET')";
+                    d.DBOperation_Update(sqlInsert);
+                    Console.WriteLine("Record inserted");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Create");
+            }
+
+            //READ
+
+
+            try { 
+                using (DataConnection d = new DataConnection("studentconn"))
+            {
+                string sqlQry = "select name,sports from student_details";
+                d.DBOperation_Read(sqlQry);
+
+                while(d.dr.Read())
+                {
+                    Console.WriteLine($"{d.dr["name"].ToString()} plays {d.dr["sports"].ToString()}" ) ;
+                       
+                }
+            }
+            }
+
+            catch
+            {
+                Console.WriteLine("Read");
+            }
+
+            //UPDATE
+
+
+            try
+            {
+                using (DataConnection d = new DataConnection("studentconn"))
+                {
+                    string sqlUpdate = "update student_details set sports='BASEBALL' where name ='PHIL'";
+                    d.DBOperation_Read(sqlUpdate);
+                    Console.WriteLine("PHIL Record updated");
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Update");
+            }
+
+            //DELETE
+            try
+            { 
+            using (DataConnection d = new DataConnection("studentconn"))
+            {
+                string sqlDelete = "delete from student_details  where name ='PHIL'";
+                d.DBOperation_Read(sqlDelete);
+                    Console.WriteLine("PHIL Record deleted");
+
+                }
+            }
+            
+
+            catch
+            {
+                Console.WriteLine("Delete");
+            }
+
+
+
+            //CALLING A STORED PROCEDURE
+
+            Console.WriteLine("*******STORED PROCEDURE**********");
+            using (DataConnection d = new DataConnection("studentconn"))
+            {
+                List<DataParam> sqlParams = new List<DataParam>();
+                List<DataParam> sqlResult;
+
+                sqlParams.Add(new DataParam("p_sports", System.Data.DbType.String, "TENNIS", System.Data.ParameterDirection.Input));
+                sqlParams.Add(new DataParam("p_girls", System.Data.DbType.String, "", System.Data.ParameterDirection.Output));
+                sqlParams.Add(new DataParam("p_boys", System.Data.DbType.String, "", System.Data.ParameterDirection.Output));
+
+                string sqlCmd = "PROC_GIRL_BOY";
+
+                sqlResult = d.DBOperation_ExecuteProcedure(sqlCmd, sqlParams);
+
+                foreach(DataParam p in sqlResult)
+                {
+                    Console.WriteLine($"{ p.paramName} : { p.paramValue}");
+                }
+
+            }
+
+                Console.ReadLine();
+        }
+
+
+
+        }
+    }
+
---
+++ common_ado.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace DBCommonADO.NET
+
+{
+    public static class CConn
+    {
+        public static string ConStr = "", ConPrvdr = "", conErr="";
+
+       public static void FindConn(string s)
+        {
+             //if connection name is not provided, fetch the first connection string  from config file      
+            if(s!=null && s!="")
+            { 
+            ConStr = ConfigurationManager.ConnectionStrings[s].ConnectionString;
+            ConPrvdr = ConfigurationManager.ConnectionStrings[s].ProviderName;
+            }
+            else
+            {
+                ConStr = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+                ConPrvdr = ConfigurationManager.ConnectionStrings[0].ProviderName;
+            }
+        }
+    }
+    public class DataConnection : IDisposable
+    {
+        public DbConnection cn;
+        public DbProviderFactory df;
+        public System.Configuration.ConnectionStringSettings connString;
+        public string s;
+        public DbDataReader dr;
+        string _reportName;
+        string _qStr;
+        
+        public DataConnection() : this("") { }
+       
+        public DataConnection(string conName) {
+            CConn.FindConn(conName);
+            s = CConn.ConPrvdr;
+            df = DbProviderFactories.GetFactory(s);
+            cn = df.CreateConnection();
+            cn.ConnectionString = CConn.ConStr;
+        }
+
+        public void DBOperation_Read(string sqlCmd)
+        {
+            try
+            {
+                if (cn.State != ConnectionState.Open)
+                {
+                    cn.Open();
+                }
+                DbCommand op = df.CreateCommand();
+                op.Connection = cn;
+                op.CommandText = sqlCmd;
+                dr = op.ExecuteReader();
+            }
+            catch (Exception er)
+            {
+                CConn.conErr = $"Error Source : {er.Source} thrown : {er.ToString()}" ;
+            }
+        }
+
+        public void DBOperation_Update(string sqlCmd)
+        {
+            try
+            {
+                if (cn.State != ConnectionState.Open)
+            {
+                cn.Open();
+            }
+            DbCommand op = df.CreateCommand();
+            op.Connection = cn;
+            op.CommandText = sqlCmd;
+            op.ExecuteNonQuery();
+            }
+            catch (Exception er)
+            {
+                CConn.conErr = $"Error Source : {er.Source} thrown : {er.ToString()}";
+            }
+        }
+
+        public object DBOperation_ExecuteScalar(string sqlCmd)
+        {
+            try { 
+            if (cn.State != ConnectionState.Open)
+            {
+                cn.Open();
+            }
+            DbCommand op = df.CreateCommand();
+            op.Connection = cn;
+            op.CommandText = sqlCmd;
+            return op.ExecuteScalar();
+        }
+            catch (Exception er)
+            {
+                CConn.conErr = $"Error Source : {er.Source} thrown : {er.ToString()}" ;
+                return er;
+            }
+}
+     public List<DataParam> DBOperation_ExecuteProcedure(string sqlCmd, List<DataParam> sqlParams)
+        {
+            if (cn.State != ConnectionState.Open)
+            {
+                cn.Open();
+            }
+            DbCommand op = df.CreateCommand();
+            op.Connection = cn;
+            op.CommandText = sqlCmd;
+            op.CommandType = CommandType.StoredProcedure;
+
+            List<string> strOut = new List<string>();
+
+            foreach (DataParam sqlparam in sqlParams)
+            {
+                DbParameter DbParam = df.CreateParameter();
+                DbParam.DbType = sqlparam.paramType;
+                DbParam.ParameterName = sqlparam.paramName;
+                DbParam.Value = sqlparam.paramValue;
+                DbParam.Size = 300;
+                DbParam.Direction = sqlparam.paramDirection;
+                op.Parameters.Add(DbParam);
+
+                if (sqlparam.paramDirection == ParameterDirection.Output)
+                {
+                    strOut.Add(sqlparam.paramName);
+                }
+
+            }
+
+
+            List<DataParam> lResult = new List<DataParam>();
+
+
+            op.ExecuteNonQuery();
+
+
+            foreach (string s in strOut)
+            {
+                lResult.Add(new DataParam(op.Parameters[s].ParameterName,op.Parameters[s].DbType, op.Parameters[s].Value.ToString(), op.Parameters[s].Direction));
+            }
+
+            return lResult;
+        }
+
+              
+        public void DBOperation_Close()
+        {
+            if (cn.State == ConnectionState.Open)
+            {
+                cn.Close();
+
+            }
+        }
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+
+
+            if (disposing)
+            {
+                if (cn.State == ConnectionState.Open)
+                {
+                    cn.Close();
+
+                }
+            }
+
+
+        }
+
+
+    }
+
+
+    public class DataParam
+    {
+        public DbType paramType;
+        public string paramValue;
+        public ParameterDirection paramDirection;
+        public string paramName;
+
+        public DataParam(string paramName,DbType paramType, string paramValue, ParameterDirection paramDirection)
+        {
+            this.paramType = paramType;
+            this.paramValue = paramValue;
+            this.paramDirection = paramDirection;
+            this.paramName = paramName;
+        }
+    }
+}
